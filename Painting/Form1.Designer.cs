eb9696956bbec7eee3a86fb400ea9366e4bf5636
Ahmad Kelany Home PC <ahmad.kelany@outlook.com>
2020-06-18 14:20:35 +0300

    Add project files.

---
+++ Painting/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Painting
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtRadius = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtDistance = new System.Windows.Forms.TextBox();
+            this.lblMakePainting = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(734, 535);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(841, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "قطر الدائرة";
+            // 
+            // txtRadius
+            // 
+            this.txtRadius.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtRadius.Location = new System.Drawing.Point(804, 82);
+            this.txtRadius.Name = "txtRadius";
+            this.txtRadius.Size = new System.Drawing.Size(100, 23);
+            this.txtRadius.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(841, 131);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "المسافة";
+            // 
+            // txtDistance
+            // 
+            this.txtDistance.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDistance.Location = new System.Drawing.Point(804, 149);
+            this.txtDistance.Name = "txtDistance";
+            this.txtDistance.Size = new System.Drawing.Size(100, 23);
+            this.txtDistance.TabIndex = 2;
+            // 
+            // lblMakePainting
+            // 
+            this.lblMakePainting.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblMakePainting.BackColor = System.Drawing.Color.Purple;
+            this.lblMakePainting.ForeColor = System.Drawing.Color.White;
+            this.lblMakePainting.Location = new System.Drawing.Point(802, 214);
+            this.lblMakePainting.Name = "lblMakePainting";
+            this.lblMakePainting.Size = new System.Drawing.Size(100, 23);
+            this.lblMakePainting.TabIndex = 3;
+            this.lblMakePainting.Text = "عمل الرسمة";
+            this.lblMakePainting.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblMakePainting.Click += new System.EventHandler(this.lblMakePainting_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(916, 559);
+            this.Controls.Add(this.lblMakePainting);
+            this.Controls.Add(this.txtDistance);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtRadius);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtRadius;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtDistance;
+        private System.Windows.Forms.Label lblMakePainting;
+    }
+}
+
---
+++ Painting/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Painting
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lblMakePainting_Click(object sender, EventArgs e)
+        {
+            decimal radius = decimal.Parse(txtRadius.Text);
+            decimal distance = decimal.Parse(txtDistance.Text);
+
+            Pen p = new Pen(Brushes.Goldenrod,1);
+            Bitmap bitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            Graphics g = Graphics.FromImage(bitmap);
+            g.FillRectangle(Brushes.White, 0, 0, pictureBox1.Width, pictureBox1.Height);
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+            g.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+            g.InterpolationMode = InterpolationMode.High;
+            pictureBox1.Image = bitmap;
+            for (int w = 10; w < pictureBox1.Width -(int )radius- 10; w += (int)distance)
+            {
+                for (int h = 10; h < pictureBox1.Height-(int)radius - 10; h += (int)distance)
+                {
+                    g.DrawEllipse(p, w, h,  (int)radius,  (int)radius);
+                }
+            }
+        }
+    }
+}
